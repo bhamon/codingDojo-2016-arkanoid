1415d742b910823fab3f591f7eb2761802bc2aa2
cryo <bhamon.cryo@gmail.com>
2016-05-11 23:04:20 +0200

    Library [glbinding] removed (broken): moved to native opengl support.

--- arkanoid/main.cpp
+++ arkanoid/main.cpp
@@ -1,10 +1,53 @@
 #include <Windows.h>
-#include <glbinding\gl\gl.h>
-#include <glbinding\Binding.h>
+#include <gl/GL.h>
 
-#define WIDTH 800
-#define HEIGHT 600
-#define CLASS_NAME "OpenGL"
+#define WINDOW_CLASS_NAME "OpenGL"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "Arkanoid"
+#define FRAMERATE_INTERVAL 30
+
+void InitGl()
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+}
+
+void ResizeGl(int p_width, int p_height)
+{
+	glViewport(0, 0, p_width, p_height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(0, 0, p_width, p_height, -1, 1);
+
+	glTranslatef(-1.0f, 1.0f, 0.0f);
+	glScalef(1.0f / static_cast<float>(p_width), -1.0f / static_cast<float>(p_height), 1.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void DrawScene()
+{
+	glPushMatrix();
+	glTranslatef(100.0f, 100.0f, 0.0f);
+
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 100.0f, 0.0f);
+	glVertex3f(100.0f, 100.0f, 0.0f);
+	glVertex3f(100.0f, 0.0f, 0.0f);
+	glEnd();
+
+	glPopMatrix();
+}
 
 LRESULT	CALLBACK WndProc(HWND p_window, UINT p_message, WPARAM p_wParam, LPARAM p_lParam)
 {
@@ -12,10 +55,19 @@ LRESULT	CALLBACK WndProc(HWND p_window, UINT p_message, WPARAM p_wParam, LPARAM 
 	{
 		case WM_CLOSE:
 			PostQuitMessage(0);
-			break;
+			return 0;
+		case WM_SIZE:
+			ResizeGl(LOWORD(p_lParam), HIWORD(p_lParam));
+			return 0;
+		case WM_KEYUP:
+			// TODO
+			return 0;
+		case WM_KEYDOWN:
+			// TODO
+			return 0;
+		default:
+			return DefWindowProc(p_window, p_message, p_wParam, p_lParam);
 	}
-
-	return DefWindowProc(p_window, p_message, p_wParam, p_lParam);
 }
 
 void KillWindow(HINSTANCE p_instance, HWND p_window, HDC p_deviceContext, HGLRC p_renderingContext)
@@ -36,14 +88,14 @@ void KillWindow(HINSTANCE p_instance, HWND p_window, HDC p_deviceContext, HGLRC 
 		DestroyWindow(p_window);
 	}
 
-	UnregisterClass(CLASS_NAME, p_instance);
+	UnregisterClass(WINDOW_CLASS_NAME, p_instance);
 }
 
 int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_args, int p_show)
 {
 	WNDCLASS wc;
 	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;    // Redraw on size, own DC window
-	wc.lpfnWndProc = (WNDPROC)WndProc;                // Message callback
+	wc.lpfnWndProc = static_cast<WNDPROC>(WndProc);   // Message callback
 	wc.cbClsExtra = 0;                                // No extra window data
 	wc.cbWndExtra = 0;                                // No extra window data
 	wc.hInstance = p_instance;                        // Set the instance
@@ -51,7 +103,7 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	wc.hCursor = LoadCursor(0, IDC_ARROW);            // Load the arrow pointer
 	wc.hbrBackground = 0;                             // No background required for OpenGL
 	wc.lpszMenuName = 0;                              // No menu
-	wc.lpszClassName = CLASS_NAME;                    // Class name
+	wc.lpszClassName = WINDOW_CLASS_NAME;             // Class name
 
 	// Registering class.
 	if(!RegisterClass(&wc))
@@ -61,19 +113,19 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	}
 
 	RECT rect;
-	rect.left = (long)0;
-	rect.right = (long)WIDTH;
-	rect.top = (long)0;
-	rect.bottom = (long)HEIGHT;
+	rect.left = static_cast<long>(0);
+	rect.right = static_cast<long>(WINDOW_WIDTH);
+	rect.top = static_cast<long>(0);
+	rect.bottom = static_cast<long>(WINDOW_HEIGHT);
 
 	// Adjust desired rect to match screen area
 	AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, FALSE, WS_EX_APPWINDOW | WS_EX_WINDOWEDGE);
 
 	// Create the window
-	HWND window = CreateWindowEx(
+	HWND hwnd = CreateWindowEx(
 		WS_EX_APPWINDOW | WS_EX_WINDOWEDGE,                         // Extended style
-		CLASS_NAME,                                                 // Class name
-		"Arkanoid",                                                 // Title
+		WINDOW_CLASS_NAME,                                          // Class name
+		WINDOW_TITLE,                               // Title
 		WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,    // Style
 		0, 0,                                                       // Position
 		rect.right - rect.left,                                     // Width
@@ -82,20 +134,20 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 		0,                                                          // No menu
 		p_instance,                                                 // Instance
 		0                                                           // Nothing to pass to WM_CREATE
-		);
+	);
 
-	if(!window)
+	if(!hwnd)
 	{
-		KillWindow(p_instance, window, 0, 0);
+		KillWindow(p_instance, hwnd, 0, 0);
 		MessageBox(NULL, "Unable to create window.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
 
 	// Retrieving device context
-	HDC deviceContext = GetDC(window);
+	HDC deviceContext = GetDC(hwnd);
 	if(!deviceContext)
 	{
-		KillWindow(p_instance, window, 0, 0);
+		KillWindow(p_instance, hwnd, 0, 0);
 		MessageBox(NULL, "Unable to create an OpenGL device context.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
@@ -123,7 +175,7 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	int pixelFormat = ChoosePixelFormat(deviceContext, &pixelFormatDescriptor);
 	if(!pixelFormat)
 	{
-		KillWindow(p_instance, window, deviceContext, 0);
+		KillWindow(p_instance, hwnd, deviceContext, 0);
 		MessageBox(NULL, "Unable to find a suitable pixel format.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
@@ -131,7 +183,7 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	// Setting pixel format
 	if(!SetPixelFormat(deviceContext, pixelFormat, &pixelFormatDescriptor))
 	{
-		KillWindow(p_instance, window, deviceContext, 0);
+		KillWindow(p_instance, hwnd, deviceContext, 0);
 		MessageBox(NULL, "Unable to set the pixel format.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
@@ -140,7 +192,7 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	HGLRC renderingContext = wglCreateContext(deviceContext);
 	if(!renderingContext)
 	{
-		KillWindow(p_instance, window, deviceContext, 0);
+		KillWindow(p_instance, hwnd, deviceContext, 0);
 		MessageBox(NULL, "Unable to create an OpenGL rendering context.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
@@ -148,28 +200,27 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 	// Activating rendering context
 	if(!wglMakeCurrent(deviceContext, renderingContext))
 	{
-		KillWindow(p_instance, window, deviceContext, renderingContext);
+		KillWindow(p_instance, hwnd, deviceContext, renderingContext);
 		MessageBox(NULL, "Unable to activate the OpenGL rendering context.", "Error", MB_OK | MB_ICONEXCLAMATION);
 		return 0;
 	}
 
-	ShowWindow(window, SW_SHOW);    // Showing window
-	SetForegroundWindow(window);    // Set to foreground
-	SetFocus(window);               // Grab keyboard focus
+	ShowWindow(hwnd, SW_SHOW);    // Showing window
+	SetForegroundWindow(hwnd);    // Set to foreground
+	SetFocus(hwnd);               // Grab keyboard focus
 
-	glbinding::Binding::initialize();
-
-	// RESIZE GL
-	// INIT GL
+	InitGl();
 
 	// Event loop
 	MSG message;
-	while(true)
+	bool run = true;
+	while(run)
 	{
 		if(PeekMessage(&message, 0, 0, 0, PM_REMOVE))
 		{
 			if(message.message == WM_QUIT)
 			{
+				run = false;
 				break;
 			}
 			else
@@ -180,12 +231,16 @@ int WINAPI WinMain(HINSTANCE p_instance, HINSTANCE p_previousInstance, LPSTR p_a
 		}
 		else
 		{
-			// DRAW SCENE
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glLoadIdentity();
+
+			DrawScene();
+
 			SwapBuffers(deviceContext);
 		}
 	}
 
-	KillWindow(p_instance, window, deviceContext, renderingContext);
+	KillWindow(p_instance, hwnd, deviceContext, renderingContext);
 
 	return message.wParam;
 }
